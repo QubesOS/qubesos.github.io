69d01c18256e154a135d0a4fce5dd77e2c10e3ae
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-05-06 04:01:46 +0200

    travis: refactor .travis.yml to a script for reuse in submodules
    QubesOS/qubes-issues#2725
    
    Squashed commit of the following:
    
    commit 74818783d78349262bc6303330e1d048c97b5752
    Author: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
    Date:   Sat May 6 03:32:51 2017 +0200
    
        travis: use trusty image
    
    commit 098c16a15ebc83d2dd7536b9e527de7a50a31da1
    Author: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
    Date:   Sat May 6 01:56:26 2017 +0200
    
        travis: run test on PR, not base version
    
        Also, always use what travis have fetched, instead of what submodule
        point at. This allow testing pushes to submodules before updating main
        repository (alternative branches etc).
    
    commit df6d29444ae35d5fcef483e826e18674b8fa02c6
    Author: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
    Date:   Sat May 6 01:55:08 2017 +0200
    
        travis: use Travis-downloaded sources for test
    
    commit 9c2a2703a0c6daf848e687ce899499f2e1d667d9
    Merge: 480ece1 0919f67
    Author: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
    Date:   Sat May 6 01:43:46 2017 +0200
    
        Merge remote-tracking branch 'jpo-bot/travis' into travis
    
    commit 0919f674153a8cd91de79e80ce638c44565e7061
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 16:19:16 2017 -0400
    
        Fix / escaping in repo url
    
    commit b0a38220945cd4d9507fd82f2193942fbca07d7d
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 16:08:43 2017 -0400
    
        Travis actually works for qubes-doc now
    
    commit 6e60d8f2843649c8dc4d091b283621f61af759c9
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 03:57:19 2017 -0400
    
        More travis fixes
    
    commit 6547e55c74ad3e9e4e4c1633070f0a62a2c4c5d1
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 03:50:30 2017 -0400
    
        Travis fixes
    
    commit bc3dda9e439cc1fdecdedec6d135d82cf7737874
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 03:39:38 2017 -0400
    
        More travis improvements
    
    commit b41033331e58589e7e3b576906e43bc07ad7645d
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 02:40:44 2017 -0400
    
        Remove -z :(
    
    commit 6a9c0350629d23a5339abde9627bf204ddcaeb7a
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 02:33:29 2017 -0400
    
        Only match repo name, not owner
    
    commit b6ddfdf3197bf1a5650e087d48ef18906e81bc3f
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 02:27:07 2017 -0400
    
        Remove unportable -z
    
    commit 119317935dae2ffece6f3daf1654fb71f4f4a1d9
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 02:15:03 2017 -0400
    
        Various travis improvements
    
    commit fe22ca07b0a1fd8e87c114051ed5868a9344da4f
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Tue Apr 18 02:14:37 2017 -0400
    
        Trace everything in travis script
    
    commit 243fbe241cd70195c9adb52121658303a69ca421
    Author: Jean-Philippe Ouellet <jpo@vt.edu>
    Date:   Mon Apr 17 23:47:09 2017 -0400
    
        Move travis tasks into script
    
        so we can clone & call it from travis hooks in other repos.

---
+++ _utils/travis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -x
+set -e
+set -o pipefail
+
+is_pr() {
+	test -n "$TRAVIS_PULL_REQUEST" -a false != "$TRAVIS_PULL_REQUEST"
+}
+
+cd "$(dirname "$0")/.."
+
+# from https://github.com/gjtorikian/html-proofer
+#     When installation speed matters, set NOKOGIRI_USE_SYSTEM_LIBRARIES to
+#     true in your environment. This is useful for increasing the speed of
+#     your Continuous Integration builds.
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
+gem install github-pages html-proofer
+
+repo_owner=${TRAVIS_REPO_SLUG%%/*}
+repo_name=${TRAVIS_REPO_SLUG#*/}
+
+# Use modules of same owner so you can test whole site before proposing PR
+git config --file .gitmodules --get-regexp '.*\.url' \
+| while read repo url; do
+	owner_url="${url/github.com\/[^\/]*\//github.com/${repo_owner}/}"
+	: url=$url
+	: repo_owner=$repo_owner
+	: owner_url=$owner_url
+	git config --file .gitmodules "$repo" "$owner_url"
+done
+
+git submodule update --init --recursive
+
+if is_pr; then
+    echo "building original site to compare"
+    bundle exec jekyll build
+
+	echo moving old site
+	mv _site ~/old_site
+fi
+
+if [ "$repo_name" != "qubesos.github.io" ]; then
+    sub_name=$(git config --file .gitmodules --list | grep -m1 -F -- "$repo_name" | cut -d. -f2)
+    sub_path=$(git config --file .gitmodules --get -- "submodule.${sub_name}.path")
+else
+    sub_path="."
+fi
+git -C "$sub_path" fetch --update-shallow ${TRAVIS_BUILD_DIR} HEAD
+git -C "$sub_path" checkout FETCH_HEAD
+bundle exec jekyll build
+
+if [ -d ~/old_site ]; then
+	echo diffing
+	diff -ur ~/old_site ./_site || true
+fi
+
+htmlproofer ./_site --disable-external --checks-to-ignore ImageCheck --file-ignore ./_site/video-tours/index.html --url-ignore "/qubes-issues/"

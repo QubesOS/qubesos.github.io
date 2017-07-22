a3b581f11b25fdbbe322a4cb0bfa3d7796675270
Jean-Philippe Ouellet <jpo@vt.edu>
2017-07-22 12:56:42 -0400

    travis: tabs -> spaces

--- _utils/travis.sh
+++ _utils/travis.sh
@@ -5,7 +5,7 @@ set -e
 set -o pipefail
 
 is_pr() {
-	test -n "$TRAVIS_PULL_REQUEST" -a false != "$TRAVIS_PULL_REQUEST"
+    test -n "$TRAVIS_PULL_REQUEST" -a false != "$TRAVIS_PULL_REQUEST"
 }
 
 cd "$(dirname "$0")/.."
@@ -23,11 +23,11 @@ repo_name=${TRAVIS_REPO_SLUG#*/}
 # Use modules of same owner so you can test whole site before proposing PR
 git config --file .gitmodules --get-regexp '.*\.url' \
 | while read repo url; do
-	owner_url="${url/github.com\/[^\/]*\//github.com/${repo_owner}/}"
-	: url=$url
-	: repo_owner=$repo_owner
-	: owner_url=$owner_url
-	git config --file .gitmodules "$repo" "$owner_url"
+    owner_url="${url/github.com\/[^\/]*\//github.com/${repo_owner}/}"
+    : url=$url
+    : repo_owner=$repo_owner
+    : owner_url=$owner_url
+    git config --file .gitmodules "$repo" "$owner_url"
 done
 
 git submodule update --init --recursive
@@ -36,8 +36,8 @@ if is_pr; then
     echo "building original site to compare"
     bundle exec jekyll build
 
-	echo moving old site
-	mv _site ~/old_site
+    echo moving old site
+    mv _site ~/old_site
 fi
 
 if [ "$repo_name" != "qubesos.github.io" ]; then
@@ -51,8 +51,8 @@ git -C "$sub_path" checkout FETCH_HEAD
 bundle exec jekyll build
 
 if [ -d ~/old_site ]; then
-	echo diffing
-	diff -ur ~/old_site ./_site || true
+    echo diffing
+    diff -ur ~/old_site ./_site || true
 fi
 
 htmlproofer ./_site --disable-external --checks-to-ignore ImageCheck --file-ignore ./_site/video-tours/index.html --url-ignore "/qubes-issues/"

38d3fc7311aaa5360ccb8c5dd37b9d6f43948203
Andrew David Wong <adw@qubes-os.org>
2024-03-18 00:44:57 -0700

    Make initialism explicit on first use

--- _utils/sec-qsb.sh
+++ _utils/sec-qsb.sh
@@ -60,7 +60,7 @@ title: \"QSB-${QSB}: ${TITLE}\"
 categories: security
 ---
 
-We have published [Qubes Security Bulletin ${QSB}: ${TITLE}](https://github.com/QubesOS/qubes-secpack/blob/main/QSBs/qsb-${QSB}-${YEAR}.txt). The text of this QSB and its accompanying cryptographic signatures are reproduced below. For an explanation of this announcement and instructions for authenticating this QSB, please see the end of this announcement.
+We have published [Qubes Security Bulletin (QSB) ${QSB}: ${TITLE}](https://github.com/QubesOS/qubes-secpack/blob/main/QSBs/qsb-${QSB}-${YEAR}.txt). The text of this QSB and its accompanying cryptographic signatures are reproduced below. For an explanation of this announcement and instructions for authenticating this QSB, please see the end of this announcement.
 
 ## Qubes Security Bulletin $QSB
 
